14f66ee00616e521224e2d7d43ef8a805de63e63
valent <valentinus.wastu@gmail.com>
2024-02-19 11:43:48 +0700

    fix: theme config

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,13 +1,16 @@
 "use client";
 
+import { siteConfig } from "@/config/site.config";
+import hideRechartsConsoleError from "@/utils/recharts-console-error";
 import { ThemeProvider as NextThemeProvider } from "next-themes";
+
+hideRechartsConsoleError();
 
 export function ThemeProvider({ children }: React.PropsWithChildren<{}>) {
   return (
     <NextThemeProvider
-      enableSystem
-      defaultTheme="system"
-      disableTransitionOnChange
+      enableSystem={false}
+      defaultTheme={String(siteConfig.mode)}
     >
       {children}
     </NextThemeProvider>
