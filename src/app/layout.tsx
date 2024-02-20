7231be9a1a6ce38ef5cdff8fb72611395486ca9d
zildirayalfirli <zildirayalfirli@gmail.com>
2024-02-20 15:53:09 +0700

    add sign page and user page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
 import type { Metadata } from "next";
+import { Toaster } from 'react-hot-toast';
 import { ThemeProvider } from "@/components/theme-provider";
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '@/app/api/auth/[...nextauth]/auth-options';
+import AuthProvider from '@/app/api/auth/[...nextauth]/auth-provider';
 import { inter, lexendDeca } from "@/app/fonts";
 import HydrogenLayout from "@/layouts/hydrogen/layout";
 import GlobalDrawer from "@/app/shared/drawer-views/container";
 import GlobalModal from "@/app/shared/modal-views/container";
 import { cn } from "@/utils/class-names";
+import NextProgress from '@/components/next-progress';
 import "@/app/globals.css";
 
 export const metadata: Metadata = {
@@ -12,26 +17,33 @@ export const metadata: Metadata = {
   description: "Write your app description",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
   return (
     <html
-      // 💡 Prevent next-themes hydration warning
+      lang="en"
+      dir="ltr"
+      // required this one for next-themes, remove it if you are not using next-theme
       suppressHydrationWarning
     >
       <body
-        // 💡 Prevent hydration warnings caused by third-party extensions, such as Grammarly.
+        // to prevent any warning that is caused by third party extensions like Grammarly
         suppressHydrationWarning
-        className={cn(inter.variable, lexendDeca.variable, "font-inter")}
+        className={cn(inter.variable, lexendDeca.variable, 'font-inter')}
       >
-        <ThemeProvider>
-          <HydrogenLayout>{children}</HydrogenLayout>
-          <GlobalDrawer />
-          <GlobalModal />
-        </ThemeProvider>
+        <AuthProvider session={session}>
+          <ThemeProvider>
+            <NextProgress />
+            {children}
+            <Toaster />
+            <GlobalDrawer />
+            <GlobalModal />
+          </ThemeProvider>
+        </AuthProvider>
       </body>
     </html>
   );
